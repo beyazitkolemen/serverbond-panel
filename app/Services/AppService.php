4350337b13d5de20c316e4c06a45df95c69fd2d3
Beyazıt Kölemen <bkolemen@gmail.com>
2025-10-22 15:46:12 +0300

    .

---
+++ app/Services/AppService.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Services;
+
+use App\Actions\Mysql\CreateDatabaseService;
+use App\Models\Site;
+use App\Actions\Nginx\AddSiteService;
+use App\Actions\Mysql\CreateUserService;
+use App\Actions\Mysql\GrantPrivilegesService;
+use App\Actions\Php\RestartService;
+
+class AppService
+{
+    public function __construct()
+    {
+    }
+
+    /**
+     * Create app
+     * @param Site $site
+     * @return void
+     */
+    public function createApp(Site $site): void
+    {
+        // Create Nginx Site
+        $addSiteService = new AddSiteService();
+        $addSiteService->execute($site->domain, $site->type->value, $site->root_directory, $site->php_version->value);
+
+        $databaseCredentials = $this->generateDatabaseCredentials($site);
+        // Create Database
+        $createDatabaseService = new CreateDatabaseService();
+        $createDatabaseService->execute($databaseCredentials['database']);
+
+        // Create User
+        $createUserService = new CreateUserService();
+        $createUserService->execute($databaseCredentials['user'], $databaseCredentials['password'], $databaseCredentials['database'], 'ALL PRIVILEGES');
+
+        // Restart PHP-FPM
+        $restartService = new RestartService();
+        $restartService->execute($site->php_version->value);
+
+
+
+
+    }
+    public function generateDatabaseCredentials(Site $site): array
+    {
+        $databaseName = Str::random(10);
+        $databaseUser = Str::random(10);
+        $databasePassword = Str::random(10);
+
+        $site->database_name = $databaseName;
+        $site->database_user = $databaseUser;
+        $site->database_password = $databasePassword;
+        $site->save();
+
+        return [
+            'database' => $databaseName,
+            'user' => $databaseUser,
+            'password' => $databasePassword,
+        ];
+    }
+}
